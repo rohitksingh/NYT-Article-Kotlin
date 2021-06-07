1e76570e4d2fec9139912600bb1903cba95bbeaa
Rohit <r4rohit002@gmail.com>
2021-06-07 10:02:20 -0700

    Added intent and button click events

---
+++ app/src/main/java/com/rohitksingh/nytarticles_kotlin/DetailActivity.kt
@@ -0,0 +1,16 @@
+package com.rohitksingh.nytarticles_kotlin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
--- app/src/main/java/com/rohitksingh/nytarticles_kotlin/MainActivity.kt
+++ app/src/main/java/com/rohitksingh/nytarticles_kotlin/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.rohitksingh.nytarticles_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    lateinit var incrementButton : Button
+    lateinit var decrementButton : Button
+    lateinit var counterTextView: TextView
+    var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        incrementButton = findViewById(R.id.increment)
+        decrementButton = findViewById(R.id.decrement)
+        counterTextView = findViewById(R.id.counter)
+
+        incrementButton.setOnClickListener(View.OnClickListener {
+            counter++
+            counterTextView.setText(""+counter)
+        })
+
+        decrementButton.setOnClickListener(this)
+
+    }
+
+    fun openActivity(counterValue: String){
+        var intent = Intent(this, DetailActivity:: class.java)
+        intent.putExtra("COUNTER_VALUE", counterValue)
+        startActivity(intent)
+    }
+
+    override fun onClick(view: View?) {
+        openActivity(" "+counter)
     }
 
 }
