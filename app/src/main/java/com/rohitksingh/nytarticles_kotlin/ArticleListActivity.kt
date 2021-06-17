cf8c4172d348a75573ccbb77f6fec6af3ff2c7b3
Rohit <r4rohit002@gmail.com>
2021-06-17 16:37:07 -0700

    Added ArticleList Activity

---
+++ app/src/main/java/com/rohitksingh/nytarticles_kotlin/ArticleListActivity.kt
@@ -0,0 +1,12 @@
+package com.rohitksingh.nytarticles_kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArticleListActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
